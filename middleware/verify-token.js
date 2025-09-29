8416d4fc3173b6b7a6e56b8eb1659cc7e3ebdbca
Dylan Tai <dylztai@gmail.com>
2025-09-29 13:59:45 -0400

    Fixed JWT_SECRET

--- middleware/verify-token.js
+++ middleware/verify-token.js
@@ -1,15 +1,17 @@
-const jwt = require('jsonwebtoken');
+const jwt = require("jsonwebtoken");
+const dotenv = require("dotenv");
+dotenv.config();
 
 function verifyToken(req, res, next) {
   try {
-    const token = req.headers.authorization.split(' ')[1];
+    const token = req.headers.authorization.split(" ")[1];
     const decoded = jwt.verify(token, process.env.JWT_SECRET);
-    
+
     req.user = decoded.payload;
-    
+
     next();
   } catch (err) {
-    res.status(401).json({ err: 'Invalid token.' });
+    res.status(401).json({ err: "Invalid token." });
   }
 }
 

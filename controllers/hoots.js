d7dd7a3a57baaed386c0721d3825976922bcdc65
daequansession <ddae20208@gmail.com>
2025-09-29 16:00:42 -0400

    update

--- controllers/hoots.js
+++ controllers/hoots.js
@@ -1,9 +1,25 @@
 // controllers/hoots.js
-
+import { Router } from "express";
 import Hoot from "../models/hoot.js";
 
 // add routes here
 // controllers/hoots.js
+// controllers/hoots.js
+// GET Index
+export const getHoots = async (req, res) => {
+  try {
+    const hoots = await Hoot.find({})
+      .populate("author")
+      .sort({ createdAt: "desc" });
+    res.status(200).json(hoots);
+  } catch (err) {
+    res.status(500).json({ err: err.message });
+  }
+};
+
+export const getHoot = async (req, res) => {
+  // fill this out
+};
 
 // controllers/hoots.js
 
--- routes/hoot.js
+++ routes/hoot.js
@@ -5,8 +5,10 @@ import { verifyToken } from "../middleware/verify-token.js";
 const router = Router();
 
 router.post("/", verifyToken, hootController.createHoot);
-// router.get("/", );
-// router. get("/:hootId", );
+
+router.get("/", hootController.getHoots);
+
+router.get("/:hootId", hootController.getHoot);
 // router.put("/:hootId", );
 // router.delete("/:hootId");
 // router.post("/:hootId", );

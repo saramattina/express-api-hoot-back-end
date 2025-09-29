6bf6ac2bd9c201b74ced3ce624bc8cfe4ad8ac5b
Dylan Tai <dylztai@gmail.com>
2025-09-29 16:30:36 -0400

    deleted a new line

--- routes/hoot.js
+++ routes/hoot.js
@@ -8,10 +8,8 @@ const router = Router();
 router.post("/", verifyToken, hootController.createHoot);
 router.get("/", verifyToken, hootController.getHoots);
 router.get("/:hootId", verifyToken, hootController.getHoot);
-
 router.put("/:hootId", verifyToken, hootController.updateHoot);
 router.delete("/:hootId", verifyToken, hootController.deleteHoot);
 router.post("/:hootId", verifyToken, hootController.postComment);
-
 
 export default router;
